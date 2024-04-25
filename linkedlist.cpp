128149cf7de345649d74e75b6af9ba08e0050dca
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2024-04-25 15:21:08 +0700

    memasukan if

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -13,11 +13,23 @@ void addNode() {
     int nim;
     string nama;
     Node* nodeBaru = new Node();
-    cout << "Masukkan Nim: ";
+    cout << "Masukan NIM : ";
     cin >> nim;
-    cout << "Masukkan Nama: ";
+    cout << "Masukan Nama : ";
     cin >> nama;
-    nodeBaru->noMhs = nim;
-    nodeBaru->name = nama;
+    nodeBaru -> noMhs = nim;
+    nodeBaru -> name = nama;
+
+    if (START == NULL || nim <= START -> noMhs){
+        if (START != NULL && nim == START -> noMhs)
+        {
+            cout << "NIM sudah ada " << endl;
+            return;
+        }
+
+        nodeBaru -> next = START;
+        START = nodeBaru;
+        return;
+    }
 
 }
